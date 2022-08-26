480f27ea41b0062e9ae17f4b6e42e42cbc178e94
王志森 <im.wongch1sum@gmail.com>
2022-08-26 14:53:37 +0800

    commit for resolving 404 error when built

--- .umirc.ts
+++ .umirc.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'Site Name',
-  mode: 'site',
+  mode: 'doc',
+  exportStatic:{},
+  publicPath:"/blog-wongchisum",
+  base:"/blog-wongchisum"
   // more config: https://d.umijs.org/config
 });
