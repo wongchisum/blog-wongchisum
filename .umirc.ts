037d1ab01d4ffbdb8fd609eeb826cb4a5d3440a4
王志森 <im.wongch1sum@gmail.com>
2022-08-26 15:02:43 +0800

    fix public path

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'Site Name',
   mode: 'doc',
   exportStatic:{},
-  publicPath:"/blog-wongchisum",
-  base:"/blog-wongchisum"
+  publicPath:"/blog-wongchisum/",
+  base:"/blog-wongchisum/"
   // more config: https://d.umijs.org/config
 });
